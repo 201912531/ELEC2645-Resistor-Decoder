ba8649c26400facf0f6c85aec49e55ff7bd5f24d
201912531 <fpjc0983@leeds.ac.uk>
2025-11-29 15:18:50 +0000

    added basic loop menu

--- src/main.c
+++ src/main.c
@@ -1,6 +1,25 @@
-#include <studio.h>
+#include <stdio.h>
 
 int main() {
-  printf("Resistor decoding project starting...");
+
+  int running = 1;
+
+  while (running) {
+    int choice;
+  
+    printf("Resistor Decoder Buddy\n");
+    printf("1: Decode a 4-band resistor\n");
+    printf("2: Exit\n");
+    printf("Enter your choice:  \n");
+    scanf("%d", &choice);
+  
+    if (choice == 1) {
+      printf(" ");
+    } else if (choice == 2) {
+      printf("byyyeeee\n");
+      running = 0;
+    } else 
+      printf("Invalid choice. Read the menu.\n");
+  }  
 return 0;
 }
