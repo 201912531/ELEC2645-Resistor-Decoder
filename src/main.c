b13c481fc631447310934c7054b06229aa232981
201912531 <fpjc0983@leeds.ac.uk>
2025-12-03 22:33:16 +0000

    skeleton code
    - menu system adapted from unit 2.1 template
    - base structure created with working menu
    - functions not yet done

--- src/main.c
+++ src/main.c
@@ -1,25 +1,90 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
+// function prototypes
+void main_menu(void);
+int get_user_input(void);
+void select_menu_item(int input);
+void print_main_menu(void);
+void go_back_to_main(void);
 
-  int running = 1;
 
-  while (running) {
-    int choice;
-  
-    printf("Resistor Decoder Buddy\n");
-    printf("1: Decode a 4-band resistor\n");
-    printf("2: Exit\n");
-    printf("Enter your choice:  \n");
-    scanf("%d", &choice);
-  
-    if (choice == 1) {
-      printf(" ");
-    } else if (choice == 2) {
-      printf("byyyeeee\n");
-      running = 0;
-    } else 
-      printf("Invalid choice. Read the menu.\n");
-  }  
-return 0;
+// resistor decoder menu items
+void menu_resistor_decoder(void);
+void menu_show_color_table(void);
+void menu_help(void);
+
+// main function
+int main(void) {
+  main_menu();
+  return 0;
 }
+
+// menu functions
+void main_menu(void) {
+  print_main_menu();
+  int input = get_user_input();
+  select_menu_item(input);
+}
+
+int get_user_input(void) {
+  int input;
+  printf("\nSelect item: ");
+  scanf("%d", &input);
+  return input;
+}
+
+void select_menu_item(int input) {
+  switch (input) {
+    case 1:
+      menu_resistor_decoder(); 
+      break;
+    case 2: 
+      menu_show_color_table(); 
+      break;
+    case 3: 
+      menu_help(); 
+      break;
+    case 4: 
+      printf("Exiting program ... \n");
+      exit(0);
+    default:
+      printf("Invalid selection. Exiting... \n");
+      exit(1);
+  }
+}
+
+void print_main_menu(void) {
+  printf("\n Hi, I'm your resistor decoder buddy. Please choose an option from the menu items below:\n");
+  printf("1.) Automatic resistor decoder\n");
+  printf("2.) Show color table\n");
+  printf("3.) Help\n");
+  printf("4.) Exit \n");
+  printf("--------------------------\n");
+}
+
+void go_back_to_main(void) {
+  char input;
+  do {
+    printf("\n Enter X to return to the main menu: ");
+    scanf(" %c", &input);
+  } while (input != 'x' && input != 'X');
+  main_menu();
+}
+
+// menu item functions
+void menu_resistor_decoder(void) {
+  printf("\n not done");
+  go_back_to_main();
+}
+
+void menu_show_color_table(void) {
+  printf("\n not done");
+  go_back_to_main();
+}
+
+void menu_help(void) {
+  printf("\n not done");
+  go_back_to_main();
+}
+
